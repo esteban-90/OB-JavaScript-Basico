9f40c60a0f64df8237cb0f20f157f8deb880cef9
esteban-90 <estebanvm1990@gmail.com>
2022-05-29 09:01:20 -0400

    Ejercicio Sintaxis, variables y palabras reservadas de JS

---
+++ sesion02/index.js
@@ -0,0 +1,14 @@
+const misDatos = [
+  "Esteban Vera Mantero",
+  31,
+  false,
+  new Date("September 20, 1990"),
+  {
+    titulo: "Eloquent JavaScript: A Modern Introduction to Programming",
+    autor: "Marijn Haverbeke",
+    fecha: new Date(2019, 0, 1),
+    url: "http://libgen.rs/book/index.php?md5=C862395EB0ECBA23A8F51C8EDF724BBB",
+  },
+];
+
+console.log(misDatos);
