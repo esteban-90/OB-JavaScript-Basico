5b7e228931f7295915a646c0bc471286d38ae91f
esteban-90 <estebanvm1990@gmail.com>
2022-05-30 09:27:02 -0400

    Ejercicio 1 Trabajando con Sets y Objetos

---
+++ sesion07/conjuntos.js
@@ -0,0 +1,15 @@
+const nombresDeMiFamilia = new Set();
+
+nombresDeMiFamilia.add("Esteban");
+nombresDeMiFamilia.add("Ligia");
+nombresDeMiFamilia.add("Felipe");
+
+console.log(nombresDeMiFamilia);
+
+nombresDeMiFamilia.add("Esteban");
+
+console.log(nombresDeMiFamilia);
+
+nombresDeMiFamilia.add("Javascript");
+
+console.log(nombresDeMiFamilia);
