03c6dc562a842518ad7bf386e80eb3746796230f
esteban-90 <estebanvm1990@gmail.com>
2022-06-01 17:22:36 -0400

    Ejercicio Persistencia de datos en navegador

---
+++ sesion15/index.js
@@ -0,0 +1,15 @@
+const nombre = "Esteban";
+const apellido = "Vera";
+
+const yo = { nombre, apellido };
+
+// sessionStorage.setItem("yo", JSON.stringify(yo));
+// localStorage.setItem("yo", JSON.stringify(yo));
+
+// document.cookie = `yo=${JSON.stringify(yo)};expires=${new Date(
+//   2022,
+//   5,
+//   1,
+//   17,
+//   21
+// ).toUTCString()}`;
